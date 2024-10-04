0f06aa371d026ede34fdec70828b882d25796b44
alan tecuapetla <alan.tecuapetla@gmail.com>
2024-10-04 17:23:11 +0900

    ts

--- app.ts
+++ app.ts
@@ -4,3 +4,22 @@ function simpleMultiply(x:number, y:number):number {
 }
 
 console.log(simpleMultiply(2,5));
+
+
+function longestCommonPrefix(strs: string[]): string {
+  strs.sort();
+  const str1 = strs[0];
+  const str2 = strs[strs.length - 1];
+  let idx = 0;
+
+  while (idx < str1.length && idx < str2.length) {
+      if (str1.charAt(idx) == str2.charAt(idx)) {
+          idx++;
+      } else {
+          break
+      }
+  }
+  return str1.substring(0, idx);
+};
+
+console.log(longestCommonPrefix(["dog", 'Doom', 'door']));
