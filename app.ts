7529eb05f2d134cad638641bd7018a8f3b2fbee3
alan tecuapetla <alan.tecuapetla@gmail.com>
2024-10-07 19:55:46 +0900

    function

--- app.ts
+++ app.ts
@@ -28,3 +28,8 @@ function longestCommonPrefix(strs: string[]): string {
   // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
   // You may assume that each input would have exactly one solution, and you may not use the same element twice.
   // You can return the answer in any order.
+
+
+  function twoSums(nums: number[], target: number) {
+
+  }
