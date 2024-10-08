959be535da29f8da16ad1739f021f61d21769050
alan tecuapetla <alan.tecuapetla@gmail.com>
2024-10-08 17:38:28 +0900

    examples

--- app.ts
+++ app.ts
@@ -111,8 +111,19 @@ console.log(romanToInteger('IVX'));
 // Longest Common Prefix
   // Write a function to find the longest common prefix string amongst an array of strings.
 
-  // If there is no common prefix, return an empty string "".
+  // If there is no common prefix, return an empty string "".7
 
-  function longestPrefix(params:type) {
+  //   Example 1:
 
-  }
+  // Input: strs = ["flower","flow","flight"]
+  // Output: "fl"
+  // Example 2:
+
+  // Input: strs = ["dog","racecar","car"]
+  // Output: ""
+  // Explanation: There is no common prefix among the input strings.
+
+
+function longestPrefix(params:type) {
+
+}
