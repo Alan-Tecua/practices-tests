5b0f6b7b48a783904b4346ecfbf371b3e088da87
alan tecuapetla <alan.tecuapetla@gmail.com>
2024-10-08 17:37:54 +0900

    function

--- app.ts
+++ app.ts
@@ -112,3 +112,7 @@ console.log(romanToInteger('IVX'));
   // Write a function to find the longest common prefix string amongst an array of strings.
 
   // If there is no common prefix, return an empty string "".
+
+  function longestPrefix(params:type) {
+
+  }
