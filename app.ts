0890a06f367fef233c105ba2e825b93bac04d041
alan tecuapetla <alan.tecuapetla@gmail.com>
2024-10-10 18:11:04 +0900

    merge sorted lists

--- app.ts
+++ app.ts
@@ -188,3 +188,9 @@ function isValid(s: string): boolean {
 console.log(isValid('()[}')); //false
 console.log(isValid('()[]{}')); //true
 console.log(isValid('({[]})')); //true
+
+
+// Merge Two Assorted Lists:
+  // You are given the heads of two sorted linked lists list1 and list2.
+  // Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+  // Return the head of the merged linked list.
