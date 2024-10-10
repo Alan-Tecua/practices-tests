04ff1342da1fcb9296310133d6e9be5fdd11915c
alan tecuapetla <alan.tecuapetla@gmail.com>
2024-10-10 18:30:37 +0900

    function

--- app.ts
+++ app.ts
@@ -194,3 +194,7 @@ console.log(isValid('({[]})')); //true
   // You are given the heads of two sorted linked lists list1 and list2.
   // Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
   // Return the head of the merged linked list.
+
+function mergeList(list1: ListNode | null, list2: ListNode | null ): ListNode | null {
+
+}
