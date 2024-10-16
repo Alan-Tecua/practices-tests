f8d93c76910bc6972cae2d358003e8d1f79350d1
alan tecuapetla <alan.tecuapetla@gmail.com>
2024-10-16 18:21:16 +0900

    while loop

--- app.ts
+++ app.ts
@@ -321,6 +321,21 @@ function addTwoNums(l1: ListNode | null, l2: ListNode | null ): ListNode | null 
     num2 = 0,
     carry = 0,
     current = ans;
-  return newl.next
-
+    while (p1 || p2) {
+      num1 = (p1) ? p1.val : 0;
+      num2 = (p2) ? p2.val : 0;
+      if(num1 + num2+carry > 9) {
+          current.next = new ListNode(num1 + num2 + carry -10);
+          current = current.next;
+          carry = 1;
+      } else {
+          current.next = new ListNode(num1 + num2 + carry );
+          current = current.next;
+          carry = 0
+      }
+      if(p1) p1 = p1.next;
+      if(p2) p2 = p2.next;
+  }
+  if(carry) current.next = new ListNode(carry);
+  return ans.next;
 }
