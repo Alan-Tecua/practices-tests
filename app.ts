77728f412d8b8b5350a4df309e01b94afc56ba0e
alan tecuapetla <alan.tecuapetla@gmail.com>
2024-10-17 23:26:19 +0900

    Index of the First Ocurrence

--- app.ts
+++ app.ts
@@ -362,3 +362,29 @@ function addTwoNums(l1: ListNode | null, l2: ListNode | null ): ListNode | null 
 //current = current.next
 // there is also one edge case, if(carry) still exists, then we need to pass it on a new list = new ListNode(carry);
 //return the ans.next
+
+
+
+//28. Find the Index of the First Occurrence in a String
+
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+
+
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+
+// Constraints:
+
+// 1 <= haystack.length, needle.length <= 104
+// haystack and needle consist of only lowercase English characters.
